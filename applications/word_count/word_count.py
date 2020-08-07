5a7b6ea24b53e298469373e2422368c7fd7f6cd9
frances-hansen <francesjuniperhansen@users.noreply.github.com>
2020-08-07 10:42:56 -0700

    update

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,5 +1,35 @@
 def word_count(s):
     # Your code here
+    cache = {}
+    words = s.split()
+
+    if s == "":
+        print(s,"dasd")
+        return {}
+
+    for w in words:
+        print(w)
+        w = w.lower()
+        q_word = ""
+        print("QWORD_1: ",q_word)
+        for l in w:
+            if l.isalnum() or l == "\'":
+                q_word += l
+                print("QWORD_2: ",q_word)
+        if q_word in cache:
+            cache[q_word] += 1
+            #print("TESTING1: ",cache)
+        elif q_word == "":
+            return {}
+        else:
+            cache[q_word] = 1
+            print("CATCHE: \n",cache)
+    c_words = dict(cache.items())
+    return c_words
+                
+        
+
+    
 
 
 
@@ -8,3 +38,6 @@ if __name__ == "__main__":
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
     print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+
+#   " : ; , . - + = / \ | [ ] { } ( ) * ^ &
+
